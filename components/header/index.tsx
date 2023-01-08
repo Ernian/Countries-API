d9634aee4412f5b4fdef37f1bd9e710c6734cf1d
light <estarosa@list.ru>
2023-01-08 23:16:36 +0300

    switch color theme in header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,27 @@
+import { useAppContext } from '../../hooks/useAppContext'
+import { SunIcon } from '../../public/svg'
+
 export default function Header() {
-  return <h1>Header</h1>
+  const context = useAppContext()
+  const changeColorTheme = () => context?.setIsDark(!context?.isDark)
+
+  return (
+    <header
+      className={`
+      ${context?.isDark && 'bg-dark-blue'} ||
+      ${!context?.isDark && 'bg-slate-100'}
+      flex justify-between px-5 py-3`
+      }>
+      <div>
+        <h1>Where in the world?</h1>
+      </div>
+      <div
+        className='hover:cursor-pointer'
+        onClick={changeColorTheme}
+      >
+        <SunIcon />
+        <span>{context?.isDark ? 'Light Mode' : 'Dark Mode'}</span>
+      </div>
+    </header>
+  )
 }
