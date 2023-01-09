9021c54656191c3490a0fe169415ed13e42cbb17
light <estarosa@list.ru>
2023-01-09 21:58:15 +0300

    footer and main section implement, change color theme

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,26 +5,18 @@ export default function Header() {
   const context = useAppContext()
   const changeColorTheme = () => context?.setIsDark(!context?.isDark)
 
+  const bgTextClasses = `${context?.isDark && 'bg-dark-blue text-very-light-gray'} ||
+                         ${!context?.isDark && 'bg-slate-100 text-very-dark-blue-lm'}`
+
   return (
     <header
-      className={`
-      ${context?.isDark && 'bg-dark-blue'} ||
-      ${!context?.isDark && 'bg-slate-100'}
-      flex justify-between items-center px-5 py-3`
-      }>
-      <div
-        className={`
-         ${context?.isDark && 'text-very-light-gray'} ||
-         ${!context?.isDark && 'text-very-dark-blue-lm'}
-         text-2xl`
-        }>
+      className={`${bgTextClasses} flex justify-between items-center px-5 py-3`}
+    >
+      <div className='text-2xl'>
         Where in the world?
       </div>
       <div
-        className={`
-        ${context?.isDark && 'text-very-light-gray'} ||
-        ${!context?.isDark && 'text-very-dark-blue-lm'}
-        text-lg flex items-center hover:cursor-pointer`}
+        className='text-lg flex items-center hover:cursor-pointer'
         onClick={changeColorTheme}
       >
         {context?.isDark ? <SunIcon /> : <MoonIcon />}
