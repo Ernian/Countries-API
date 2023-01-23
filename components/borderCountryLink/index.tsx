76969fb0ab7b99e8f6f190d7a1916e3f31f6db56
light <estarosa@list.ru>
2023-01-23 21:37:38 +0300

    404 page, country dir,fix build bugs

--- components/borderCountryLink/index.tsx
+++ components/borderCountryLink/index.tsx
@@ -11,7 +11,7 @@ export default function BorderCountryLink({ country }: { country: IBorderCountry
 
   return (
     <Link
-      href={`/${country.ccn3}`}
+      href={`/country/${country.ccn3}`}
       className={`${bgTextClasses} inline-block mx-1 mt-2 px-3 py-2 
        h-10 rounded-md hover:shadow-lg`}
     >
